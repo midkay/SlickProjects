aee56c652c51d787f4da90afa8b5bc5f2305d2a9
Zakk Roberts <midkay@gmail.com>
2012-11-30 21:46:27 -0800

    minor changes
    - removed unnecessary commit
    - modified mario level a bit

--- src/marioRipoff/MarioRipoff.java
+++ src/marioRipoff/MarioRipoff.java
@@ -10,7 +10,6 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.Sound;
 
 public class MarioRipoff extends BasicGame {
 
