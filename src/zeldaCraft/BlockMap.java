68b9b5da14e8f8227b3b38c62499f0cf3d1838db
Brandon Roth <b.g.roth@hotmail.com>
2012-12-01 08:03:57 -0800

    ZeldaCraft Map-collision testing
    Far more efficient, yet slight less effective, collision testing!

--- src/zeldaCraft/BlockMap.java
+++ src/zeldaCraft/BlockMap.java
@@ -1,6 +1,6 @@
 package zeldaCraft;
 
-import java.util.ArrayList;
+import java.util.*;
 
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
@@ -11,14 +11,16 @@ public class BlockMap extends ZeldaCraft {
 	public static int mapWidth;
 	public static int mapHeight;
 	
-	public static ArrayList<Object> entities;
+	//public static ArrayList<Object> entities;
+	public static Map<Integer, Block> entities;
 	
 	//square shaped tile
 	//Coords indicate top left, top right, bottom right, and bottom left respectively
-	private int square[] = {0,0, 31,0, 31,31, 0,31};
+	private int square[] = {0,0, 32,0, 32,32, 0,32};
 	
 	public BlockMap (String mapData) throws SlickException {
-		entities = new ArrayList<Object>();
+		//entities = new ArrayList<Object>();
+		entities = new HashMap<Integer, Block>();
 		tmap = new TiledMap (mapData, "res/zeldaCraft/");
 		mapWidth = tmap.getWidth() * tmap.getTileWidth();
 		mapHeight = tmap.getHeight() * tmap.getTileHeight();
@@ -31,8 +33,17 @@ public class BlockMap extends ZeldaCraft {
 				boolean blocked = "true".equals (tmap.getTileProperty (tileID, "blocked", "false"));
 				
 				//if there is collision, create a poly around it
-				if (blocked == true)
-					entities.add (new Block (x * 32, y * 32, square, "square"));
+				if (blocked == true) {
+					//entities.add (new Block (x * 32, y * 32, square, "square"));
+					int temp = x * 32 * 10000;
+					temp +=  y * 32;
+					
+					System.out.println(temp);
+					entities.put(temp, new Block (x * 32, y * 32, square, "square"));
+
+					//System.out.println(x * 32 + " " + y * 32);
+				}
+				
 			}
 		}
 		
--- src/zeldaCraft/ZeldaCraft.java
+++ src/zeldaCraft/ZeldaCraft.java
@@ -15,7 +15,6 @@ public class ZeldaCraft extends BasicGame {
 	public static final int SCREEN_WIDTH = 1024;
 	public static final int SCREEN_HEIGHT = 576;
 	
-	
 	public float worldX = SCREEN_WIDTH / 2;
 	public float worldY = SCREEN_HEIGHT / 2;
 	
@@ -31,9 +30,9 @@ public class ZeldaCraft extends BasicGame {
 	@Override
 	public void init(GameContainer container) throws SlickException {
 		map = new BlockMap("res/zeldaCraft/level.tmx");
+		
 		player = new Image ("res/zeldaCraft/link_front.gif");
 		p1 = new PlayerZelda (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-		
 		playerPoly = new Polygon(new float[]{
 				p1.posX(), p1.posY(),
 				p1.posX()+32, p1.posY(),
@@ -98,40 +97,52 @@ public class ZeldaCraft extends BasicGame {
 		if (input.isKeyDown(Input.KEY_ESCAPE))
 			System.exit(0);
 		//end key input
-		
-		/*
-		// start scrolling
-		worldX += (playerX - playerOldX);
-		playerX = playerOldX;
-		
-		worldY += (playerY - playerOldY);
-		playerY = playerOldY;
-		// end scrolling
-		*/
 	}
 	
 	public boolean entityCollisionWith() throws SlickException {
-		for (int i = 0; i < BlockMap.entities.size(); i++) {
-			Block entity1 = (Block) BlockMap.entities.get(i);
-			if (playerPoly.intersects (entity1.poly))
-				return true;
-		}     
+//		for (int i = 0; i < BlockMap.entities.size(); i++) {
+//			Block entity1 = (Block) BlockMap.entities.get(i);
+//			if (playerPoly.intersects (entity1.poly))
+//				return true;
+//		}     
+//		return false;
+
+		int x = (int) p1.posX();
+		int y = (int) p1.posY();
+
+		int temp1 = (x - x%32) * 10000 + (y - y%32);
+		int temp2 = (x - x%32 + 32) * 10000 + (y - y%32);
+		int temp3 = (x - x%32) * 10000 + (y - y%32 + 32);
+		int temp4 = (x - x%32 + 32) * 10000 + (y - y%32 + 32);
+
+		Block entity1 = (Block) BlockMap.entities.get(temp1);
+		Block entity2 = (Block) BlockMap.entities.get(temp2);
+		Block entity3 = (Block) BlockMap.entities.get(temp3);
+		Block entity4 = (Block) BlockMap.entities.get(temp4);
+		
+		if(entity1 != null && playerPoly.intersects (entity1.poly))
+			return true;
+		if(entity2 != null && playerPoly.intersects (entity2.poly))
+			return true;
+		if(entity3 != null && playerPoly.intersects (entity3.poly))
+			return true;
+		if(entity4 != null && playerPoly.intersects (entity4.poly))
+			return true;
+		
 		return false;
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		BlockMap.tmap.render (0, 0);
+		BlockMap.tmap.render (0, 0, 0, 0, 32, 32);
 		
 		// draw overlays (text etc)
 		g.drawString("ZeldaCraft v.CHoDE", 10, 30);
-		
 		
 		// player coords
 		g.drawString("playerX: " + p1.posX(), 10, 50);
 		g.drawString("playerY: " + p1.posY(), 10, 70);
 		//player polygon box
 		g.draw (playerPoly);
-		
 		
 		player.draw (p1.posX(), p1.posY());
 	}
