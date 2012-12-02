9ba90544f4f06032d1b43478703a3006fe12f888
Zakk Roberts <midkay@gmail.com>
2012-12-02 05:34:30 -0800

    Big ZeldaCraft performance update
    We now only render the map tiles that are visible on screen (not as
    straightforward as it might sound). Net result: 4x performance boost

--- src/zeldaCraft/ZeldaCraft.java
+++ src/zeldaCraft/ZeldaCraft.java
@@ -10,42 +10,46 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class ZeldaCraft extends BasicGame {
-	
+
 	public static TiledMap map;
 	public static int tileSize = 32;
-	
+
 	public static final int SCREEN_WIDTH = 1024;
 	public static final int SCREEN_HEIGHT = 576;
-	
+
 	public float worldX = SCREEN_WIDTH / 2;
 	public float worldY = SCREEN_HEIGHT / 2;
-	
+
 	public static boolean[][] collideTiles;
-	
+
 	private static PlayerZelda p1;
 	private static Image player = null;
 	public final float playerSpeed = (float) 0.2;
-	
+
 	public ZeldaCraft() {
-		super("A minecraft, minicraft, zelda ripoff (sorry mojang/nintendo plz no sue)");
+		super(
+				"A minecraft, minicraft, zelda ripoff (sorry mojang/nintendo plz no sue)");
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		map = new TiledMap ("res/zeldaCraft/level.tmx", "res/zeldaCraft/");
-		
-		player = new Image ("res/zeldaCraft/link_front.gif");
-		p1 = new PlayerZelda (map.getWidth() * tileSize / 2, map.getHeight() * tileSize / 2);		
+		map = new TiledMap("res/zeldaCraft/level.tmx", "res/zeldaCraft/");
+
+		player = new Image("res/zeldaCraft/link_front.gif");
+		p1 = new PlayerZelda(map.getWidth() * tileSize / 2, map.getHeight()
+				* tileSize / 2);
 		collideTiles = new boolean[map.getWidth()][map.getHeight()];
-		
-		for(int i=0; i<map.getWidth(); i++) {
-			for(int j=0; j<map.getHeight(); j++) {
-				int tileID = map.getTileId (i, j, 0);
-	
-				collideTiles[i][j] = "true".equals (map.getTileProperty (tileID, "blocked", "false"));
-				
-				if("true".equals (map.getTileProperty (tileID, "spawn", "false")))
-					p1.setPos(i*tileSize, j*tileSize);
+
+		for (int i = 0; i < map.getWidth(); i++) {
+			for (int j = 0; j < map.getHeight(); j++) {
+				int tileID = map.getTileId(i, j, 0);
+
+				collideTiles[i][j] = "true".equals(map.getTileProperty(tileID,
+						"blocked", "false"));
+
+				if ("true"
+						.equals(map.getTileProperty(tileID, "spawn", "false")))
+					p1.setPos(i * tileSize, j * tileSize);
 			}
 		}
 	}
@@ -54,52 +58,58 @@ public class ZeldaCraft extends BasicGame {
 	public void update(GameContainer gc, int delta) throws SlickException {
 		Input input = gc.getInput();
 
-		//start key input
+		// start key input
 		if (input.isKeyDown(Input.KEY_W)) {
 			player = new Image("res/zeldaCraft/link_back.gif");
-			p1.moveY (-playerSpeed * delta);
+			p1.moveY(-playerSpeed * delta);
 		}
-		
+
 		if (input.isKeyDown(Input.KEY_A)) {
 			player = new Image("res/zeldaCraft/link_lside.gif");
-			p1.moveX (-playerSpeed * delta);
+			p1.moveX(-playerSpeed * delta);
 		}
-		
+
 		if (input.isKeyDown(Input.KEY_S)) {
 			player = new Image("res/zeldaCraft/link_front.gif");
-			p1.moveY (playerSpeed * delta);
+			p1.moveY(playerSpeed * delta);
 		}
-		
+
 		if (input.isKeyDown(Input.KEY_D)) {
 			player = new Image("res/zeldaCraft/link_rside.gif");
-			p1.moveX (playerSpeed * delta);
+			p1.moveX(playerSpeed * delta);
 		}
-		
+
 		if (input.isKeyDown(Input.KEY_ESCAPE))
 			System.exit(0);
-		//end key input
-		
+		// end key input
+
 		// WORLD SCROLLING CODE
-		// move the world view left if player's global x coordinate nears the right edge
-		worldX = (float) (SCREEN_WIDTH/2 - p1.playerX);
-		worldY = (float) (SCREEN_HEIGHT/2 - p1.playerY);
-		
+		// move the world view left if player's global x coordinate nears the
+		// right edge
+		worldX = (float) (SCREEN_WIDTH / 2 - p1.playerX);
+		worldY = (float) (SCREEN_HEIGHT / 2 - p1.playerY);
+
 		p1.tick();
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-		map.render ((int) worldX, (int) worldY);
-		
+		// render the map efficiently -- draw only the tiles we need drawn
+		// warning: this is confusing!!
+		map.render(-tileSize + (int) (worldX % tileSize), -tileSize
+				+ (int) (worldY % tileSize), (int) (-worldX / tileSize) - 1,
+				(int) (-worldY / tileSize) - 1, SCREEN_WIDTH / tileSize + 2,
+				SCREEN_HEIGHT / tileSize + 2);
+
 		// draw overlays (text etc)
 		g.drawString("ZeldaCraft v.CHoDE", 10, 30);
-		
+
 		// player coords
 		g.drawString("playerX: " + p1.posX(), 10, 50);
 		g.drawString("playerY: " + p1.posY(), 10, 70);
-		
-		player.draw (SCREEN_WIDTH/2, SCREEN_HEIGHT/2);
-	}	
-	
+
+		player.draw(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	}
+
 	public static void main(String[] args) throws SlickException {
 		AppGameContainer app = new AppGameContainer(new ZeldaCraft());
 
